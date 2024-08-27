4a1e92b0f39dffd7db5e73ec261e03d6ed39dc0b
Omar Orga <orgaomar8@gmail.com>
2024-08-27 22:22:55 +0800

    "Updated VSCode settings and removed prog.c file"

---
+++ 14. pointer as function return/prog14.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+void printhelloworld()
+{
+    printf("Hello world\n");
+}
+int* add(int* d, int* e)
+{
+    // int* f = malloc(sizeof(int));
+    int f = (*d) + (*e);
+    printf("(add) f = %d\n",f);
+    return &f;
+}
+
+int main()
+{
+    int a = 2, b=3;
+    int* ptr = add(&a,&b);
+    // printhelloworld();
+    printf("main add = %d\n",(add(&a,&b)));
+    printf("main f = %d\n",ptr);
+    printf(" main sum = %d\n",*ptr);
+    // free(ptr);
+}
