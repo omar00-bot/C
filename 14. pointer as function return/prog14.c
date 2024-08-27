44e3eaa19c264657c60132d6ce683462ec40a2bd
Omar Orga <orgaomar8@gmail.com>
2024-08-27 23:01:27 +0800

    "Fixed memory allocation and return in add function, added free in main, and swapped printhelloworld call"

--- 14. pointer as function return/prog14.c
+++ 14. pointer as function return/prog14.c
@@ -6,19 +6,19 @@ void printhelloworld()
 }
 int* add(int* d, int* e)
 {
-    // int* f = malloc(sizeof(int));
-    int f = (*d) + (*e);
-    printf("(add) f = %d\n",f);
-    return &f;
+    int* f = malloc(sizeof(int));
+    *f = (*d) + (*e);
+    printf("(add) f = %d\n",*f);
+    return f;
 }
 
 int main()
 {
     int a = 2, b=3;
     int* ptr = add(&a,&b);
-    // printhelloworld();
+    printhelloworld();
     printf("main add = %d\n",(add(&a,&b)));
     printf("main f = %d\n",ptr);
     printf(" main sum = %d\n",*ptr);
-    // free(ptr);
+    free(ptr);
 }
